8bc432bb071155c65edc1273a206c2c6d73098d7
Kalvin <kalvin.daniel56300@gmail.com>
2022-11-25 14:33:07 +0100

    increment decrement

--- api/code/Controller/Product.js
+++ api/code/Controller/Product.js
@@ -42,10 +42,30 @@ const deleteId = (req, res) => {
 		}))
 }
 
+const addStock = (req, res) => {
+	let id = req.params.id;
+	Product.findOneAndUpdate({ _id: id }, {$inc: { stock: req.body.stock }})
+		.then((result) => res.status(200).json(result))
+		.catch((error) => res.status(400).json({
+			message: error?.message || "Une erreur est survenue"
+		}))
+}
+
+const removeStock = (req, res) => {
+	let id = req.params.id;
+	Product.findOneAndUpdate({ _id: id }, {$inc: { stock: -req.body.stock }})
+		.then((result) => res.status(200).json(result))
+		.catch((error) => res.status(400).json({
+			message: error?.message || "Une erreur est survenue"
+		}))
+}
+
 module.exports = {
     create,
 	getId,
 	getAll,
 	put,
 	deleteId,
+	addStock,
+	removeStock,
 }
